9b2ed7b5abb62785d9b3f02b4293b73f91c782d4
DeividasKLms <deividaskorsakas4@gmail.com>
2026-06-17 22:14:51 +0300

    added zustand version of anecdote app

---
+++ part-6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,24 @@
+import { useAnecdotes } from '../store'
+
+const AnecdoteList = () => {
+  const anecdotes = useAnecdotes()
+  const vote = id => {
+    console.log('vote', id)
+  }
+
+  return (
+    <div>
+      {anecdotes.map(anecdote => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
