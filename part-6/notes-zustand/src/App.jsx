28129ddb57ffaab10670b038655e51a3b84f3004
DeividasKLms <deividaskorsakas4@gmail.com>
2026-07-19 14:02:51 +0300

    added tests

--- part-6/notes-zustand/src/App.jsx
+++ part-6/notes-zustand/src/App.jsx
@@ -1,8 +1,16 @@
+import { useNoteActions } from './components/store'
+import { useEffect } from 'react'
 import NoteForm from './components/NoteForm'
 import NoteList from './components/NoteList'
 import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
+  const { initialize } = useNoteActions()
+
+  useEffect(() => {
+    initialize()
+  }, [initialize])
+
   return (
     <div>
       <NoteForm />
